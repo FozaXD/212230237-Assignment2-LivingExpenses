3f4605c22dc096d527ca3e285644fc769a0df5d9
Alexander Fotheringham <fozaxd@Alexanders-MacBook-Pro.local>
2017-05-03 15:42:06 +1000

    Added Bill Details view (tested with test button).

---
+++ Living Expenses/Living Expenses/BillDetails.swift
@@ -0,0 +1,35 @@
+//
+//  BillDetails.swift
+//  Living Expenses
+//
+//  Created by Alexander Fotheringham on 3/5/17.
+//  Copyright © 2017 Alexander Fotheringham. All rights reserved.
+//
+
+import UIKit
+
+class BillDetailsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
